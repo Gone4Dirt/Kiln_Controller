6b548104724698fc50eebcfe5b31f0aa152e4b33
Matt Kear <matthewnkear@gmail.com>
2022-12-30 23:20:05 +0000

    log_viewer: Add variables to be plotted

--- log_viewer.py
+++ log_viewer.py
@@ -6,8 +6,12 @@ MILLIS_2_MIN = 1/60000
 
 filename = fd.askopenfilename()
 
-time_min = []
+log_time_min = []
+therm_profile_time_min = []
 desired_temp = []
+measured_time_min = []
+board_temp = []
+measure_temp = []
 
 with open(filename, 'r') as file:
     lines = csv.reader(file)
@@ -23,8 +27,15 @@ with open(filename, 'r') as file:
             continue
         # print(data)
 
-        time_min.append(float(data[0])*MILLIS_2_MIN)
-        desired_temp.append(float(data[1]))
+        log_time_min.append(float(data[0])*MILLIS_2_MIN)
+
+        therm_profile_time_min.append(float(data[1])*MILLIS_2_MIN)
+        desired_temp.append(float(data[2]))
+
+        measured_time_min.append(float(data[3])*MILLIS_2_MIN)
+        board_temp.append(float(data[4]))
+        measure_temp.append(float(data[5]))
+
 
         # count += 1
 
@@ -35,7 +46,18 @@ with open(filename, 'r') as file:
 
 
 # Plot the data
-fig, ax = plt.subplots(figsize=(10, 5))
+fig, ax1 = plt.subplots(figsize=(10, 5))
+
+ax1.plot(therm_profile_time_min, desired_temp, label="Desired", linestyle='-', marker='', linewidth=1.5, color=[0,0,1])
+ax1.plot(measured_time_min, board_temp, label="Board", linestyle='-', marker='', linewidth=1.5, color=[0.5,0.5,0.5])
+ax1.plot(measured_time_min, measure_temp, label="Measured", linestyle='-', marker='', linewidth=1.5, color=[0,0,0])
+
+ax1.set_xlabel('Time Elapsed (min)')
+ax1.set_ylabel('Temp (deg C)')
+
+ax1.legend(loc='upper right')
+
+
 
 ax.plot(time_min, desired_temp, label="Des temp", linestyle='-', marker='', linewidth=1.5, color=[0,0,1])
 
